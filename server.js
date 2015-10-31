93f72917d678e07b8ce23867df0cbd70eafec773
Aditi Anomita Mohanty <aditi.anomita@gmail.com>
2015-10-31 14:14:09 +0530

    yolo

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var port = process.env.PORT || 7335;
 
 var server = app.listen( port , function (){
 	console.log("Listening on port " + server.address().port);
+	console.log("This should work!");
 });
 
 app.use('/', express.static(__dirname + '/app'));
